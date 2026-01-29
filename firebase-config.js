31cd29c65a1b982498f2f413060951734568b0ce
mrhamzaboudrifa-crypto <mrhamzaboudrifa@gmail.com>
2026-01-29 11:32:44 +0000

    Add Firebase configuration settings

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "cousins-rummy.firebaseapp.com",
+  databaseURL: "https://cousins-rummy-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cousins-rummy",
+  storageBucket: "cousins-rummy.appspot.com",
+  messagingSenderId: "752064222902",
+  appId: "1:752064222902:web:8b7888d57f6368072c3817",
+  measurementId: "G-3JG23RWD0L"
+};
